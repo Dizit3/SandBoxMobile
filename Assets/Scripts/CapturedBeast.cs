c3e83a90dbae3b8d720d7e2c4334b739d1506be0
Dizit3 <dizit3@gmail.com>
2023-09-22 10:02:43 +0300

    1

--- Assets/Scripts/CapturedBeast.cs
+++ Assets/Scripts/CapturedBeast.cs
@@ -1,11 +1,12 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
 public class CapturedBeast : MonoBehaviour
 {
-
     private NavMeshAgent agent;
     [SerializeField] private Transform player;
+    public static int activatedUnits = 0;
+
     private void Awake()
     {
         transform.GetComponentInParent<CellController>().OnCellOpen += CellOpenHandler;
@@ -16,13 +17,24 @@ public class CapturedBeast : MonoBehaviour
         agent = gameObject.AddComponent<NavMeshAgent>();
         agent.enabled = true;
 
-      
     }
 
     private void Update()
     {
-        if (agent != null) 
-        agent.SetDestination(player.position + new Vector3(2,0,2));
+        if (agent != null)
+        {
+            // Вычисляем позицию в сетке на основе номера активированного юнита
+            int row = activatedUnits / 3;  // Предположим, что у нас 3 колонны
+            int col = activatedUnits % 3;
+
+            Vector3 targetPosition = player.position + new Vector3(row * 2, 0, col * 2);
+            agent.SetDestination(targetPosition);
+        }
     }
 
+    private void OnDestroy()
+    {
+        // Уменьшаем счетчик активированных юнитов, если юнит уничтожается
+        activatedUnits--;
+    }
 }
---
+++ Assets/Scripts/GridForUnits/GridPoints.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GridPoints : MonoBehaviour
+{
+    [SerializeField] private GameObject[] unitPositions;
+
+
+
+
+
+}
